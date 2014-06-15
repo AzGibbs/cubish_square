e215b6ee0f12c32fc212ca58c6b7723d5db8f107
Pavol Klacansky <pavol@klacansky.com>
2014-06-15 12:10:12 +0100

    Add example of coding style used in project

---
+++ coding_style.cpp
@@ -0,0 +1,32 @@
+/* Block comments only allowed  */
+
+/* first include external header files, ascending order */
+#include <iostream>
+#include <string>
+
+/* related header are put in blocks */
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
+/* internal header files go last, C++ files use .hpp extension */
+#include "cube.hpp"
+
+
+/* constants are UPPER_CASE */
+const int SUPER_CONSTANT = 10;
+
+/* two empty lines before start of class */
+/* classname starts with capital, camelcase */
+class ClassName {
+/* first put public methods and variables, as they should be visible to user */
+public:
+    ClassName();
+    ~ClassName();
+
+    /* always use const if method does not change class */
+    int compute_cubishness_factor(const ClassName &other_class) const;
+
+private:
+    /* use underscore to separate words in variables */
+    int cube_factor;
+};
