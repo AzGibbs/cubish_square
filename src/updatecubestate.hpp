e21ba449c7d6d64f7c5e3acbce1c81bcaffa9d42
Aaron Gibbon <aaron_gibbon@hotmail.co.uk>
2014-07-06 12:46:38 +0100

    add function to change cubes velocity based on key press

---
+++ src/updatecubestate.hpp
@@ -0,0 +1,11 @@
+#ifndef UPDATECUBESTATE_HPP
+#define UPDATECUBESTATE_HPP
+
+#include <iostream>
+#include <vector>
+#include "cube.hpp"
+#include "input.hpp"
+
+void update_master_state(std::vector<Cube> &cubes, const KEY key);
+
+#endif
